d5044ccad25bf842e8eaaa1183c8212e5d3f43e3
mika <msakai@netprotections.co.jp>
2016-06-17 01:17:14 +0000

    second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,10 +19,25 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+     @user = User.find(params[:id])
+  end
+  
+  def update
+       @user = User.find(params[:id])
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to @user , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
  private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :password, :area,
                                  :password_confirmation)
   end
 end
